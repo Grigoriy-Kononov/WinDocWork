e41974ab3bc55efa1d54347f76bdc8ebe6ca4138
Grigoriy-Kononov <kononoff88@gmail.com>
2024-01-16 23:49:31 +0300

    1

--- WordHelper.cs
+++ WordHelper.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using Word = Microsoft.Office.Interop.Word;
 
 namespace WinDocWork
